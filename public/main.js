a2f7e5fdee17ec12bc072aeb0d5bb05501e9175b
Pablo Ortiz Gervilla <pablogervilla123@gmail.com>
2023-02-01 11:06:31 +0100

    Uploaded
    Responsive for the landing page

---
+++ public/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+const imagenes = document.getElementsByTagName('img');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-brightness-high-fill');
+    if(this.classList.toggle('bi-moon')){
+        body.className = 'light-theme';
+        body.style.transition = '.5s';
+        for(let i = 0; i < imagenes.length; i ++){
+            let imagen = (imagenes[i].src.split("dark").join("light"));
+            imagenes[i].setAttribute('src',imagen);
+            }
+        }
+    else{
+        body.className = 'dark-theme';
+        body.style.transition = '.5s';
+        for(let i = 0; i < imagenes.length; i ++){
+
+            let imagen = (imagenes[i].src.split("light").join("dark"));
+            imagenes[i].setAttribute('src',imagen);
+            }
+    }
+});
+
+
+document.querySelector('.toggle')
+        .addEventListener('click', function() {
+  this.classList.toggle('activate');
+  document.querySelector('.menu').classList.toggle('menu-column');
+});
