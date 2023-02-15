dbd79e6a0f52ef106cd3f3b3b6b13acb7812ca2a
Pablo Ortiz Gervilla <pablogervilla123@gmail.com>
2023-02-15 10:51:10 +0100

    Revision About Me

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-
-
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 const imagenes = document.getElementsByTagName('img');
 
+
+// TAKES CARE OF THE LIGHT AND DARK THEME WORKS
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-brightness-high-fill');
     if(this.classList.toggle('bi-moon')){
@@ -25,19 +25,18 @@ toggle.addEventListener('click', function(){
     }
 });
 
-
-document.querySelector('.toggle')
-        .addEventListener('click', function() {
+// CHANGE THE CLASS TO THE NAV 
+document.querySelector('.toggle').addEventListener('click', function() {
   this.classList.toggle('activate');
   document.querySelector('.menu').classList.toggle('menu-column');
 });
 
-
+// CONTROL WHEN THE ANIMATION OF THE BARS STARTS TO OCCUR IN ABOUT ME
 let done = false;
 $(document).on("scroll", function(){
     if(!done){
 
-        if ($(document).scrollTop() > 700) {
+        if ($(document).scrollTop() > 400) {
             console.log($(document).scrollTop());
             for(let i = 1; i<6; i++)
                 $(".fillcont"+i).css("animation-play-state", "running");//.fadeOut();
