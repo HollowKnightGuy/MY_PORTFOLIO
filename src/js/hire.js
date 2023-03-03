3e49feab62ef4db8ba4e1ebe9ca27d1cbc067385
Pablo Ortiz Gervilla <98289923+HollowKnightGuy@users.noreply.github.com>
2023-03-03 18:59:30 +0100

    update

---
+++ src/js/hire.js
@@ -0,0 +1,27 @@
+
+
+const input1 = document.getElementById('nameinput');
+const input2 = document.getElementById('phoneinput');
+const input3 = document.getElementById('emailinput');
+const input4 = document.getElementById('messageinput');
+
+
+
+input1.addEventListener('blur', function () { 
+    input1.previousElementSibling.style.color = 'var(--text3-color)';
+})
+
+
+input2.addEventListener('blur', function () { 
+    input2.previousElementSibling.style.color = 'var(--text3-color)';
+})
+
+input3.addEventListener('blur', function () { 
+    input3.previousElementSibling.style.color = 'var(--text3-color)';
+})
+
+input4.addEventListener('blur', function () { 
+    input4.previousElementSibling.style.color = 'var(--text3-color)';
+})
+
+AOS.init();
--- src/js/main.js
+++ src/js/main.js
@@ -40,37 +40,12 @@ $(document).on("scroll", function(){
         }
     }
     if ($(document).scrollTop() > 1400) {
-        console.log($(document).scrollTop());
         for(let j = 1; j < 3; j++){
-            console.log(  $(".lang-bars"+j));
             $(".lang-bars"+j).css("animation-play-state", "running");
         }
     }
 });
 
 
-const input1 = document.getElementById('nameinput');
-const input2 = document.getElementById('phoneinput');
-const input3 = document.getElementById('emailinput');
-const input4 = document.getElementById('messageinput');
-
-
-
-input1.addEventListener('blur', function () { 
-    input1.previousElementSibling.style.color = 'var(--text3-color)';
-})
-
-
-input2.addEventListener('blur', function () { 
-    input2.previousElementSibling.style.color = 'var(--text3-color)';
-})
-
-input3.addEventListener('blur', function () { 
-    input3.previousElementSibling.style.color = 'var(--text3-color)';
-})
-
-input4.addEventListener('blur', function () { 
-    input4.previousElementSibling.style.color = 'var(--text3-color)';
-})
 
 AOS.init();
