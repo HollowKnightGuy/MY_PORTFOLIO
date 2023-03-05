88f3acb4fd2aa514e0fb89567cba3d8c77b0f0d4
Pablo Ortiz Gervilla <98289923+HollowKnightGuy@users.noreply.github.com>
2023-03-05 16:04:43 +0100

    proyectoy finalizadp

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,30 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 const imagenes = document.getElementsByTagName('img');
+const sources = document.getElementsByTagName('source');
+let heightcalc;
+
+if(screen.width > 1000 && screen.width <= 1300){
+    heightcalc = 750;
+}
+
+
+if(screen.width > 1301 && screen.width <= 1600){
+    heightcalc = 750;
+}
+
+
+else if(screen.width > 1601 && screen.width <= 1900){
+    heightcalc = 710;
+}
+
+else if(screen.width > 1901 && screen.width <= 2200){
+    heightcalc = 800;
+}
+
+else if(screen.width > 2201){
+    heightcalc = 950;
+}
 
 
 // TAKES CARE OF THE LIGHT AND DARK THEME WORKS
@@ -13,6 +37,10 @@ toggle.addEventListener('click', function(){
             let imagen = (imagenes[i].src.split("dark").join("light"));
             imagenes[i].setAttribute('src',imagen);
             }
+        for(let i = 0; i < sources.length; i ++){
+            let imagen = (sources[i].srcset.split("dark").join("light"));
+            sources[i].setAttribute('srcset',imagen);
+            }
         }
     else{
         body.className = 'dark-theme';
@@ -24,24 +52,19 @@ toggle.addEventListener('click', function(){
             }
     }
 });
-
 // CHANGE THE CLASS TO THE NAV 
 document.querySelector('.toggle').addEventListener('click', function() {
-  this.classList.toggle('activate');
-  document.querySelector('.menu-mine').classList.toggle('menu-column');
+    this.classList.toggle('activate');
+    document.querySelector('.menu-mine').classList.toggle('menu-column');
 });
 
 // CONTROL WHEN THE ANIMATION OF THE BARS STARTS TO OCCUR IN ABOUT ME
 
 $(document).on("scroll", function(){
-    if ($(document).scrollTop() > 700) {
+    console.log($(document).scrollTop());
+    if ($(document).scrollTop() > heightcalc) {
         for(let i = 1; i < 6; i++){
             $(".fillcont"+i).css("animation-play-state", "running");//.fadeOut();
-        }
-    }
-    if ($(document).scrollTop() > 1400) {
-        for(let j = 1; j < 3; j++){
-            $(".lang-bars"+j).css("animation-play-state", "running");
         }
     }
 });
